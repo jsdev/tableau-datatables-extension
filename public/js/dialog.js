bc616b80666b6b4a0af6316d719b62e1bc17fe5b
Anthony "Ryan" Delorie <jsdev@users.noreply.github.com>
2023-02-13 22:54:19 -0500

    Update dialog.js
    removing jquery dependency

--- public/js/dialog.js
+++ public/js/dialog.js
@@ -1,78 +1,47 @@
 'use strict';
 
+var uiSettings = [
+   'compact',
+  'hover',
+  'nowrap',
+  'order-column',
+  'row-border',
+  'stripe',
+  'include-table-name'
+];
+
+var pluginSettings = [
+  'export-clipboard',
+  'export-excel',
+  'export-csv',
+  'export-pdf',
+  'export-print'
+];
+
 (function () {
 
   $(document).ready(function () {
-    tableau.extensions.initializeDialogAsync().then(function (openPayload) {
-      buildDialog();
-    });
+
   });
+  
+  document.addEventListener('DOMContentLoaded', wireExtension, false);
+
+  function wireExtension () {
+      tableau.extensions.initializeDialogAsync().then(function (openPayload) {
+        buildDialog();
+      });
+  }
 
   // We bulid the dialogue box and ensure that settings are read from the
   // UI Namespace and the UI is updated.
   function buildDialog() {
     var worksheetName = tableau.extensions.settings.get("worksheet");
+
     if (worksheetName != undefined) {
-      // We restore the look and feel settings.
-      if (tableau.extensions.settings.get("compact") == "Y") {
-        $("#compact").prop("checked", true);
-      } else {
-        $("#compact").prop("checked", false);
-      }
-      if (tableau.extensions.settings.get("hover") == "Y") {
-        $("#hover").prop("checked", true);
-      } else {
-        $("#hover").prop("checked", false);
-      }
-      if (tableau.extensions.settings.get("nowrap") == "Y") {
-        $("#nowrap").prop("checked", true);
-      } else {
-        $("#nowrap").prop("checked", false);
-      }
-      if (tableau.extensions.settings.get("order-column") == "Y") {
-        $("#order-column").prop("checked", true);
-      } else {
-        $("#order-column").prop("checked", false);
-      }
-      if (tableau.extensions.settings.get("row-border") == "Y") {
-        $("#row-border").prop("checked", true);
-      } else {
-        $("#row-border").prop("checked", false);
-      }
-      if (tableau.extensions.settings.get("stripe") == "Y") {
-        $("#stripe").prop("checked", true);
-      } else {
-        $("#stripe").prop("checked", false);
-      }
-
-      $("#include-table-name").prop("checked", tableau.extensions.settings.get("include-table-name") == "Y" ? true : false);
-
-      // We restore the plugin settings.
-      if (tableau.extensions.settings.get("export-clipboard") == "Y") {
-        $("#export-clipboard").prop("checked", true);
-      } else {
-        $("#export-clipboard").prop("checked", false);
-      }
-      if (tableau.extensions.settings.get("export-excel") == "Y") {
-        $("#export-excel").prop("checked", true);
-      } else {
-        $("#export-excel").prop("checked", false);
-      }
-      if (tableau.extensions.settings.get("export-csv") == "Y") {
-        $("#export-csv").prop("checked", true);
-      } else {
-        $("#export-csv").prop("checked", false);
-      }
-      if (tableau.extensions.settings.get("export-pdf") == "Y") {
-        $("#export-pdf").prop("checked", true);
-      } else {
-        $("#export-pdf").prop("checked", false);
-      }
-      if (tableau.extensions.settings.get("export-print") == "Y") {
-        $("#export-print").prop("checked", true);
-      } else {
-        $("#export-print").prop("checked", false);
-      }
+      // We restore the look and feel and plugin settings.
+      [].concat(uiSettings).concat(pluginSettings).forEach(setting => {
+        document.getElementById(setting).checked = tableau.extensions.settings.get(setting) === "Y";
+      });
     }
 
     // Populate the worksheet drop down with a list of worksheets.
@@ -127,9 +96,7 @@
     var underlying = $("#underlying").val();
 
     // Get the worksheet object for the specified names.
-    var worksheet = worksheets.find(function (sheet) {
-      return sheet.name === worksheetName;
-    });
+    var worksheet = worksheets.find(sheet => sheet.name === worksheetName);
 
     // If underlying is 1 then get Underlying, else get Summary. Note that the columns will
     // look different if you have summary or underlying.
@@ -191,79 +158,28 @@
     // open the configuration dialogue.
     // https://datatables.net/examples/styling/
     var tableClass = "";
-    if ($("#compact").is(":checked")) {
-      tableClass += " compact";
-      tableau.extensions.settings.set("compact", "Y");
-    } else {
-      tableau.extensions.settings.set("compact", "N");
-    }
-    if ($("#hover").is(":checked")) {
-      tableClass += " hover";
-      tableau.extensions.settings.set("hover", "Y");
-    } else {
-      tableau.extensions.settings.set("hover", "N");
-    }
-    if ($("#nowrap").is(":checked")) {
-      tableau.extensions.settings.set("nowrap", "Y");
-    } else {
-      tableClass += " nowrap";
-      tableau.extensions.settings.set("nowrap", "N");
-    }
-    if ($("#order-column").is(":checked")) {
-      tableClass += " order-column";
-      tableau.extensions.settings.set("order-column", "Y");
-    } else {
-      tableau.extensions.settings.set("order-column", "N");
-    }
-    if ($("#row-border").is(":checked")) {
-      tableClass += " row-border";
-      tableau.extensions.settings.set("row-border", "Y");
-    } else {
-      tableau.extensions.settings.set("row-border", "N");
-    }
-    if ($("#stripe").is(":checked")) {
-      tableClass += " stripe";
-      tableau.extensions.settings.set("stripe", "Y");
-    } else {
-      tableau.extensions.settings.set("stripe", "N");
-    }
-    if ($("#include-table-name").is(":checked")) {
-        tableClass += " include-table-name";
-        tableau.extensions.settings.set("include-table-name", "Y");
-    } else {
-        tableau.extensions.settings.set("include-table-name", "N");
-    }
+
+    uiSettings.forEach(setting => {
+      if (document.getElementById(setting).checked) {
+        tableClass += ` ${setting}`;
+        tableau.extensions.settings.set(setting, "Y");
+      } else {
+        tableau.extensions.settings.set(setting, "N");
+      }
+    });
 
     tableau.extensions.settings.set("table-classes", tableClass);
 
     // Saves the individual Y and N for the plugin settings so that we can restore this
     // when you open the configuration dialogue.
     // https://datatables.net/extensions/buttons/examples/html5/simple.html
-    if ($("#export-clipboard").is(":checked")) {
-      tableau.extensions.settings.set("export-clipboard", "Y");
-    } else {
-      tableau.extensions.settings.set("export-clipboard", "N");
-    }
-    if ($("#export-excel").is(":checked")) {
-      tableau.extensions.settings.set("export-excel", "Y");
-    } else {
-      tableau.extensions.settings.set("export-excel", "N");
-    }
-    if ($("#export-csv").is(":checked")) {
-      tableau.extensions.settings.set("export-csv", "Y");
-    } else {
-      tableau.extensions.settings.set("export-csv", "N");
-    }
-    if ($("#export-pdf").is(":checked")) {
-      tableau.extensions.settings.set("export-pdf", "Y");
-    } else {
-      tableau.extensions.settings.set("export-pdf", "N");
-    }
-    if ($("#export-print").is(":checked")) {
-      tableau.extensions.settings.set("export-print", "Y");
-    } else {
-      tableau.extensions.settings.set("export-print", "N");
-    }
+
+    pluginSettings.forEach(setting => {
+      tableau.extensions.settings.set(
+        setting,
+        document.getElementById(setting).checked ? "Y" : "N"
+      );
+    });
 
     // This gets the column information and saves the column order and column name.
     // For example, if you have a data source with three columns and then reorder
